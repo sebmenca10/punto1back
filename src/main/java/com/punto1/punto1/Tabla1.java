ed8fdc5f3887b6b61ea724b4e0129bd10d5cdeea
sebmenca10 <sebmenca3@gmail.com>
2023-05-03 09:40:09 -0500

    Second commit

--- src/main/java/com/punto1/punto1/Tabla1.java
+++ src/main/java/com/punto1/punto1/Tabla1.java
@@ -1,8 +1,8 @@
 package com.punto1.punto1;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name="tabla1")
